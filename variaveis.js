46f949b65947fe88eaea6594c6ef4a2d2a954bd8
Lucas Ritter <lucasjritter@gmail.com>
2022-11-23 15:11:14 -0300

    Fim de variáveis parte I

--- variaveis.js
+++ variaveis.js
@@ -1,2 +1,12 @@
-//Declarar
 
+let name = "Sim"
+console.log("Váriavel chegou aqui?", name)
+
+{
+    let name = "Não"
+    console.log("Váriavel chegou aqui?", name)
+    
+}
+
+console.log("Váriavel chegou aqui?", name)
+console.log("Isso é escopo global vs local em prática")
