743e624b8efa2dde6e6c9361dbce9946a7fd917c
Lucas Ritter <lucasjritter@gmail.com>
2022-11-25 12:24:28 -0300

    Aula - Praticando

---
+++ ex1.js
@@ -0,0 +1,58 @@
+var person;
+console.log( person)
+
+person = {
+    name: "Moacir",
+    age: 30,
+    weight: 69,
+    isAdmin: false,
+    isAMan: true,
+}
+
+let human = {    
+    name: "Moacir",
+    age: 30,
+    weight: 69,
+    isAdmin: false,
+    isAMan: true,
+}
+
+console.log(`O humano ${human.name} tem o peso ${human.weight}`);
+
+let humans = [
+    human
+]
+console.log("humans", humans)
+console.log(humans[0].name)
+
+let newHuman = {
+    name: "Lucas",
+    age: 26,
+    weight: 63,
+    isAdmin: false,
+    isAMan: true,
+};
+
+humans = [
+    human,
+    newHuman
+]
+
+console.log(humans)
+console.log(humans[1])
+
+let a 
+console.log(a)
+{
+    let a = 2
+    console.log(a)
+}
+console.log(a)
+
+const c = 10
+console.log(c)
+{
+    const c = 20
+    console.log(c)
+}
+console.log(c)
